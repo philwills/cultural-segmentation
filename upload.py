bff507c7145965924e495f90828cf0e3715afc1e
Phil Wills <otherphil@gmail.com>
2010-03-27 22:32:59 +0000

    Changed app name, as now actually deployed and remembered to add BeautifulSoup

--- upload.py
+++ upload.py
@@ -2,9 +2,9 @@ import urllib
 import urllib2
 import csv
 
-reader = csv.DictReader(open('/tmp/Segments_and_area.csv'), fieldnames=['region', 'county', 'district', 'name', 'base', 'urban_arts_eclectic', 'traditional_culture_vultures', 'fun_fashion_and_friends', 'mature_explorers', 'dinner_and_a_show', 'family_and_community_focused', 'bedroom_djs', 'mid-life_hobbyists', 'retired_arts_and_crafts', 'time_poor_dreamers', 'a_quiet_pint_with_the_match', 'older_and_home_bound'])
+reader = csv.DictReader(open('Segments_and_area.csv'), fieldnames=['region', 'county', 'district', 'name', 'base', 'urban_arts_eclectic', 'traditional_culture_vultures', 'fun_fashion_and_friends', 'mature_explorers', 'dinner_and_a_show', 'family_and_community_focused', 'bedroom_djs', 'mid-life_hobbyists', 'retired_arts_and_crafts', 'time_poor_dreamers', 'a_quiet_pint_with_the_match', 'older_and_home_bound'])
 
-url = 'http://localhost:8080/upload'
+url = 'http://cultural-segmentation.appspot.com/upload'
 
 for line in reader:
     data = urllib.urlencode(line)
